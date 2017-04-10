250b7dd40484c3ba687368e7a93da233a0e71b25
Sank Senathira <xmp.dev011@gmail.com>
2017-04-10 14:50:40 -0400

    Minified our HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
     concat = require('gulp-concat');
 
 var env,
@@ -72,7 +73,7 @@ gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
-  gulp.watch(htmlSources, ['html']);
+  gulp.watch('builds/development/*.html', ['html']);
   gulp.watch(jsonSources, ['json']);
 });
 
@@ -84,7 +85,11 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-  gulp.src(htmlSources)
+  gulp.src('builds/development/*.html')
+/*    .pipe(gulpif(env === 'production', minifyHTML())) */
+    .pipe(minifyHTML())
+/*    .pipe(gulpif(env === 'production', gulp.dest(outputDir))) */
+    .pipe(gulp.dest(outputDir))
     .pipe(connect.reload())
 });
 
